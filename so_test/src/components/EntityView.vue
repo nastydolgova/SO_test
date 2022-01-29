e8a22ef8344a469569d4c88775339b3b17361779
nastydolgova <nasty.dolgova@gmail.com>
2022-01-30 01:04:01 +0300

    begin

---
+++ so_test/src/components/EntityView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="padding-left: 10px">
+    <input type="checkbox" :id="item.id" :name="item.name" :checked="checkChecked()">
+         {{ item.name }}
+    <template v-if="item.Children != null">
+      <EntityView v-for="value in item.Children" :key="value.id" :item="value"/>
+       <!-- v-on="$listeners" -->
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class EntityView extends Vue {
+  @Prop()
+  item: any;
+
+  checkChecked(){
+    if(this.item.Children){
+      this.item.Children.forEach((child: any) => {
+          if (child.checked != this.item.checked)
+          return false
+          // добавить indeterminate
+          // добавить условие что если в массиве у всех дочерних равны между собой но не равны родителю - родитель меняется
+          // добавить модель
+          // добавить оформление
+        }
+      )
+    } else {
+      return this.item.checked
+    }
+  }
+}
+</script>
+<style scoped lang="scss"></style>
---
+++ so_test/src/views/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="home">
+    <EntityView v-for="item in rules" :key="item.id" :item="item" />
+     <!-- @changeChecked="changeChecked" -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import EntityView from "../components/EntityView.vue";
+
+@Component({
+  components: {
+    EntityView,
+  },
+})
+export default class Home extends Vue {
+  rules: any[] = [
+    {
+      id: 1,
+      parentId: null,
+      name: "Редактирование профиля",
+      checked: true,
+      Children: [
+        {
+          id: 11,
+          parentId: 1,
+          name: "Изменение профиля",
+          checked: true,
+          Children: [
+            {
+              id: 121,
+              parentId: 11,
+              name: "Удаление",
+              checked: false,
+              Children: null,
+            },
+            {
+              id: 122,
+              parentId: 11,
+              name: "Добавление",
+              checked: true,
+              Children: null,
+            },
+          ],
+        },
+        {
+          id: 12,
+          parentId: 1,
+          name: "Редактирование имени",
+          checked: false,
+          Children: null,
+        },
+      ],
+    },
+    {
+      id: 2,
+      parentId: null,
+      name: "Управление сущностью",
+      checked: true,
+      Children: [
+        {
+          id: 21,
+          parentId: 2,
+          name: "Удаление сущности",
+          checked: true,
+          Children: null,
+        },
+        {
+          id: 22,
+          parentId: 2,
+          name: "Добавление сущности",
+          checked: true,
+          Children: null,
+        },
+      ],
+    },
+    {
+      id: 3,
+      parentId: null,
+      name: "Режим админа",
+      checked: true,
+    },
+  ];
+}
+</script>
